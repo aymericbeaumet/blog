65298a3086e380bd382fc74ebb1cf5c53af11fd4
Aymeric Beaumet <hi@aymericbeaumet.com>
2022-07-28 01:06:17 +0200

    update devto script

--- scripts/devto.js
+++ scripts/devto.js
@@ -9,25 +9,29 @@ const prettier = require('prettier');
 const filepath = process.argv[2];
 const filecontent = fs.readFileSync(filepath);
 const { content, data } = matter(filecontent);
+const canonicalURL = `https://aymericbeaumet.com/${path.basename(path.dirname(filepath))}`;
 
 if (data.tags.length > 4) {
 	throw new Error('dev.to supports at most 4 tags');
 }
 
-const newcontent = prettier.format(content, {
+const newcontent = `
+_Originally published at ${canonicalURL}._
+
+${prettier.format(content, {
 	parser: 'markdown',
 	proseWrap: 'never',
-});
+})}`;
 
 const newdata = {
 	published: true,
 	title: data.title,
 	date: datefns.format(data.date, "yyyy-MM-dd '00:00:00 UTC'"),
 	tags: data.tags,
-	canonical_url: `https://aymericbeaumet.com/${path.basename(path.dirname(filepath))}`,
+	canonical_url: canonicalURL,
 	// cover_image:
 	// description:
 	// series:
 };
 
-console.log(matter.stringify(newcontent, newdata));
+process.stdout.write(matter.stringify(newcontent, newdata).trimEnd());
